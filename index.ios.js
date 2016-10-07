6b9e987179dbe1efc2489e5deb53e5b8e2d1cdb7
Brian Eng <brian@brianeng.com>
2016-10-07 07:54:55 -0500

    just about finished

--- index.ios.js
+++ index.ios.js
@@ -5,48 +5,85 @@ var React = require('react')
 var ReactNative = require('react-native')
 
 // React Native components you want to use
-var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} = ReactNative
+var { AppRegistry, Image, ScrollView, StyleSheet, Text, TouchableHighlight, View } = ReactNative
 
-var BuyflixNative = React.createClass({
+var Firebase = require('firebase')
+var firebase = Firebase.initializeApp({
+  databaseURL: "https://buyflix-d7037.firebaseio.com/"
+})
+var database = firebase.database()
+var moviesRef = database.ref('/movies')
+
+var Movie = React.createClass({
+  onMovieClicked: function() {
+    this.props.onMovieClicked(this.props.id)
+  },
   render: function() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          KIEI-924 is pretty awesome!
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <TouchableHighlight onPress={this.onMovieClicked} style={styles.thumbnail}>
+        <Image source={{uri: this.props.movie.poster}}
+               style={styles.thumbnail}
+               onClick={this.onMovieClicked} />
+      </TouchableHighlight>
     )
   }
 })
 
+var BuyflixNative = React.createClass({
+  onMovieClicked: function(movieId) {
+    var moviesWithoutRemoved = this.state.movies.filter(function(movie) {
+      return movie.key !== movieId
+    })
+    this.setState({movies: moviesWithoutRemoved})
+    moviesRef.set(moviesWithoutRemoved)
+  },
+  componentDidMount: function() {
+    moviesRef.on('value', function(snapshot) {
+      this.setState({ movies: snapshot.val() })
+    }.bind(this))
+  },
+  getInitialState: function() {
+    return {
+      movies: []
+    }
+  },
+  renderMovie: function(movie) {
+    return <Movie key={movie.key} movie={movie} id={movie.key} onMovieClicked={this.onMovieClicked} />
+  },
+  render: function() {
+    if (this.state.movies == null) {
+      return (
+        <View style={styles.noMovies}><Text>No movies!</Text></View>
+      )
+    } else {
+      return (
+        <View style={styles.scrollView}>
+          {this.state.movies.map(this.renderMovie)}
+        </View>
+      )
+    }
+  }
+})
+
 var styles = StyleSheet.create({
-  container: {
+  noMovies: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    alignItems: 'center'
   },
-  welcome: {
-    fontSize: 20,
-    fontFamily: 'Iowan Old Style',
-    color: '#3366cc',
-    textAlign: 'center',
-    margin: 10,
+  scrollView: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    padding: 20,
+    backgroundColor: '#000000',
+    height: 1000
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  thumbnail: {
+    width: 70,
+    height: 104,
+    margin: 5
+  }
 })
 
 AppRegistry.registerComponent('BuyflixNative', () => BuyflixNative)
